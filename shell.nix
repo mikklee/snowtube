8ce185b00c95fc7cc86aaac8c25330184ea7c959
Michlee <03tackler.dreamer@icloud.com>
2025-10-31 19:51:56 +0100

    fix: issues with rust-analyzer on nixos

--- shell.nix
+++ shell.nix
@@ -1,26 +1,28 @@
-{ pkgs ? import <nixpkgs> {} }:
-  let
-    libPath = with pkgs; lib.makeLibraryPath [
-      libGL
-      libxkbcommon
-      wayland
+{ pkgs ? import <nixpkgs> { } }:
+let
+  libPath = with pkgs; lib.makeLibraryPath [
+    libGL
+    libxkbcommon
+    wayland
+  ];
+in
+{
+  devShell = with pkgs; mkShell {
+    buildInputs = [
+      rustup
+      rust-analyzer
+      gcc
+      nil
+      nixd
     ];
-  in {
-    devShell = with pkgs; mkShell {
-      buildInputs = [
-        rustup
-        gcc
-        nil
-        nixd
-      ];
 
-      RUST_LOG = "debug";
-      LD_LIBRARY_PATH = libPath;
-      OPENSSL_DIR= "${pkgs.openssl.dev}";
-      OPENSSL_LIB_DIR= "${openssl.out}/lib";
-      
-      shellHook = ''
-        export PATH="$HOME/.cargo/bin:$PATH"
-      '';
-    };
-  }
+    RUST_LOG = "debug";
+    LD_LIBRARY_PATH = libPath;
+    OPENSSL_DIR = "${pkgs.openssl.dev}";
+    OPENSSL_LIB_DIR = "${openssl.out}/lib";
+
+    shellHook = ''
+      export PATH="$HOME/.cargo/bin:$PATH"
+    '';
+  };
+}
