deb1b14b5ee24e34c2f8d02ccbe90b932d3afd0c
Michlee <code@meow.fi>
2026-01-29 20:14:00 +0100

    fix: use master branch yt-dlp
    They have started changing stuff again

--- shell.nix
+++ shell.nix
@@ -1,4 +1,15 @@
 { pkgs ? import <nixpkgs> { }
+, yt-dlp-master ? pkgs.yt-dlp.overrideAttrs (old: {
+    version = "master";
+    src = pkgs.fetchFromGitHub {
+      owner = "yt-dlp";
+      repo = "yt-dlp";
+      rev = "master";
+      sha256 = "sha256-nw/L71aoAJSCbW1y8ir8obrFPSbVlBA0UtlrxL6YtCQ=";
+    };
+    patches = [ ];
+    postPatch = "";
+  })
 ,
 }:
 let
@@ -79,7 +90,7 @@ in
           nil
           nixd
           mpv
-          yt-dlp
+          yt-dlp-master
         ]
         ++ platformDeps
         ++ gstreamerDeps;
